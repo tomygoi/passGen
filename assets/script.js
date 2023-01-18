cb9c163e9d8d3d08e8dd208384e0b2ebb643f4aa
Tony Trinh <tonytrinh911@gmail.com>
2023-01-18 17:43:31 -0500

    created alert for invalid inputs

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,32 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+var criteria = {
+  length: null,
+  lowercase: false,
+  uppercase: false,
+  numberic: false,
+  special: false
+};
+
+criteria.length = prompt("Enter a password legnth (8-128 characters)");
+criteria.lowercase = prompt("Include lowercase characters?");
+criteria.length = prompt("Include uppercase characters?");
+criteria.length = prompt("Include numeric characters?");
+criteria.length = prompt("Include special characters?");
+
+if (isNaN(criteria.length) ||
+    criteria.length < 8 ||
+    criteria.length > 128 ||
+    !criteria.lowercase && 
+    !criteria.uppercase &&
+    !criteria.numeric && 
+    !criteria.special)
+    {
+      alert("Invalid input, please try again.");
+      return;
+    }
+
   var password = "";
   var characters = {
     lowercase: "abcdefghijklmnopqrstuvwxyz",
